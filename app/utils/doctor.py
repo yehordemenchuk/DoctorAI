a4d7db5d1ba3c446b3ed62113a7532331933e691
Yehor Demenchuk <yehor.demenchuk@student.tuke.sk>
2025-01-25 22:12:31 +0100

    improve answer grammar validation

--- app/utils/doctor.py
+++ app/utils/doctor.py
@@ -4,24 +4,37 @@ DoctorAI is not a medical institution and does not replace professional medical 
 import g4f
 from app.config import Config
 
-def get_response(specialization: str, user_prompt: str) -> str:
+def get_response(system_prompt: str, user_prompt: str) -> str:
     return g4f.ChatCompletion.create(
         model = Config.MODEL,
         messages=[
-            {"role": "system", "content": f"""You are a {specialization} specialist, 
-                                           who giving personal recommendations to patients"""},
+            {"role": "system", "content": system_prompt},
             {"role": "user", "content": user_prompt}
         ]
     )
 
+def validate_text(text: str) -> str:
+    return g4f.ChatCompletion.create(
+        model=Config.MODEL,
+        messages=[
+            {"role": "system", "content": """Validate the grammar and spelling of the following text and if 
+                                          it`s correct just return it: """},
+            {"role": "user", "content": text}
+        ]
+    )
+
 def personal_consultation(language: str, specialization: str, question: str) -> str:
-    return get_response(
-        specialization,
-        f"Give answer to a question on {language}: {question}"
+    return validate_text(
+        get_response(
+f"You are a {specialization} specialist, who giving personal recommendations to patients",
+  f"Give answer to a question on {language}: {question}"
+        )
     )
 
 def analyzing_medical_document(language: str, specialization: str, text: str) -> str:
-    return get_response(
-        specialization,
-        f"Analyze following document and give answer on{language}: {text}"
+    return validate_text(
+        get_response(
+            specialization,
+ f"Analyze following document and give answer on{language}: {text}"
+        )
     )
