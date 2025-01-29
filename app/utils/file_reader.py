af9055f6bfed7713afac7fa019ad2abf1a2e2e2f
Yehor Demenchuk <yehor.demenchuk@student.tuke.sk>
2025-01-29 16:08:14 +0100

    improve code quality

--- app/utils/file_reader.py
+++ app/utils/file_reader.py
@@ -6,10 +6,10 @@ import docx
 
 def read_pdf(filename: str) -> str:
     with pdfplumber.open(filename) as pdf:
-        return "\n".join([page.extract_text() for page in pdf.pages if page.extract_text()])
+        return "\n".join(page.extract_text() for page in pdf.pages if page.extract_text())
 
 def read_docx(filename: str) -> str:
-    return "\n".join([para.text for para in docx.Document(filename).paragraphs])
+    return "\n".join(para.text for para in docx.Document(filename).paragraphs)
 
 def read_file(filename: str) -> str:
     if filename.endswith('docx'):
