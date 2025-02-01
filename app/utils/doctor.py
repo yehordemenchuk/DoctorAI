62f8b305bb2a3bd2f313f9a0e0daf418c6104795
Yehor Demenchuk <yehor.demenchuk@student.tuke.sk>
2025-02-01 11:05:14 +0100

    improve prompt quality

--- app/utils/doctor.py
+++ app/utils/doctor.py
@@ -9,16 +9,16 @@ def get_response(specialization: str, user_prompt: str) -> str:
         model = Config.MODEL,
         messages = [
             {"role": "system", "content": f"Real professional {specialization} doctor"},
-            {"role": "user", "content": f"""If you were a real qualified doctor, specialized 
-                                        on {specialization}""" + user_prompt}
+            {"role": "user", "content": f"""Act as real, qualified and professional doctor 
+                                        specialized on {specialization}.""" + user_prompt}
         ]
     )
 
 def personal_consultation(language: str, specialization: str, question: str) -> str:
     return get_response(
         specialization,
-        f"""What clear, accurate, and ethical advice strictly in {language} advices did you provide, 
-        ensuring grammatical correctness, words not in answering language or incorrect symbols, 
+        f"""Provide clear, accurate, and ethical advice strictly on {language} advices, 
+        ensuring grammatical correctness, remove words not in answering language or invalid chars, 
         to the patient's question {question}."""
     )
 
@@ -27,7 +27,9 @@ def analyzing_medical_document(document_language: str,
     return get_response(
         specialization,
         f"""Analyze following medical document on {document_language} 
-        and give answer on {answer_language} and before answering 
-        validate grammar, spelling and remove invalid chars, if all is
-        correct, just return an answer: {text}"""
+        and give answer on {answer_language} and before returning an answer 
+        validate answer grammar, spelling and remove invalid chars, that can`t be read by human, and 
+        words not in answer language, and you can`t answer "I can`t help you" 
+        if answer is by your specialization if all is correct, 
+        just return an answer, don`t write about it`s corectness: {text}."""
     )
